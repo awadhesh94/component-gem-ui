4b2ddef775bdcb8d12af6b0266331744981cbbfa
awadhesh94 <awadhesh31st@outlook.com>
2023-04-11 01:28:51 +0530

    story component added

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { CardProps } from '../../types/card';
+import { UnderLine } from '../Common/UnderLine';
+
+export const Card: React.FC<CardProps> = ({
+  className,
+  icon,
+  subTitle,
+  title,
+  description,
+}) => {
+  return (
+    <div className="flex">
+      <div className="cardContainer">
+        <div className={`cardWrapper ${className}`}>
+          <div className="card-icon">{icon}</div>
+          <div className="grid mt-16">
+            <span className="font-brandMedium">{subTitle}</span>
+            <h3 className="text-2xl font-brandMedium capitalize">{title}</h3>
+            {description && (
+              <>
+                <UnderLine />
+                <p className="font-brandLight leading-normal text-sm">
+                  {description}
+                </p>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/button/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const Button = () => {
-  return <div className="font-brandHeading text-brand-purple">Button</div>;
-};
