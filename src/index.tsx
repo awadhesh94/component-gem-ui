6e5b5048fd0be16d3cd95632cad3285199d07a26
awadhesh94 <awadhesh31st@outlook.com>
2023-04-10 13:50:48 +0530

    code update

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -8,7 +7,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    App
   </React.StrictMode>,
 );
 
