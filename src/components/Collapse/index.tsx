92eab851626a4bf7d55389d29124429ab3832665
awadhesh94 <awadhesh31st@outlook.com>
2023-04-12 20:22:05 +0530

    collapse component storybook

---
+++ src/components/Collapse/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { AddIcon } from '../../icons/AddIcon';
+import { MinusIcon } from '../../icons/MinusIcon';
+import { CollapseProps } from '../../types/Collapse';
+
+export const Collapse: React.FC<CollapseProps> = ({
+  title,
+  description,
+  className,
+  open,
+  setOpen,
+}) => {
+  const hideIcon = <AddIcon width={25} height={25} />;
+  const showIcon = <MinusIcon width={25} height={25} />;
+
+  const icon = open ? showIcon : hideIcon;
+  const randerClass = open && 'bg-brand-lightBlue';
+
+  return (
+    <div className={`flex flex-col max-w-lg m-8 py-9 rounded-2xl ${randerClass} ${className}`}>
+      <button className="flex justify-between items-center px-10" onClick={() => setOpen(!open)}>
+        <h4 className="font-brandBold text-2xl text-inherit leading-snug">{title}</h4>
+        <div>{icon}</div>
+      </button>
+      {open && <div className="px-10 mt-5 font-light text-base leading-normal">{description}</div>}
+    </div>
+  );
+};
---
+++ src/stories/Collapse.stories.tsx
@@ -0,0 +1,21 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Collapse } from '../components/Collapse';
+
+import { title, description } from '../content/collapse.json';
+
+const meta = {
+  title: 'Component/Collapse',
+  component: Collapse,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Collapse>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const _Collapse: Story = {
+  args: {
+    title,
+    description,
+    open: false,
+  },
+};
