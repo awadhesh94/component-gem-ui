5c5fc79d500efa4a57ca2d589c4be617f4978f14
awadhesh94 <awadhesh31st@outlook.com>
2023-04-18 20:12:43 +0530

    card code update with test case

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,20 +2,40 @@ import React from 'react';
 import { CardProps } from '../../types/Card';
 
 export const Card: React.FC<CardProps> = ({ className, icon, subTitle, title, description }) => {
+  const mainStyle = `max-w-[350px] ${icon && 'min-h-[500px]'}`;
+  const customStyle = `${icon && 'gap-10'} ${className}`;
+
   return (
-    <section className="min-h-[500px] p-0 m-0 flex max-w-[350px] h-auto">
-      <div className={`p-8 flex justify-between flex-col gap-10 rounded-2xl ${className}`}>
-        <div className="cardIcon">{icon}</div>
-        <div className="flex flex-col justify-between gap-4">
-          <div className="cardHeader">
-            <span className="font-brandMedium">{subTitle}</span>
-            <h4 className="font-brandBold text-2xl text-inherit leading-snug">{title}</h4>
+    <section className={`flex break-words relative min-w-0 h-auto ${mainStyle}`}>
+      <div className={`p-8 flex flex-col justify-between rounded-2xl ${customStyle}`}>
+        {icon && (
+          <div className="cardIcon" data-testid="card-icon">
+            {icon}
           </div>
+        )}
+        <div className="flex flex-col justify-between">
+          <div className="cardHeader">
+            {subTitle && (
+              <span className="font-brandMedium" data-testid="card-subTitle">
+                {subTitle}
+              </span>
+            )}
+            {title && (
+              <h4
+                className="font-brandBold text-2xl text-inherit leading-snug"
+                data-testid="card-title"
+              >
+                {title}
+              </h4>
+            )}
+          </div>
+          {title && description && (
+            <hr className="border-[1px] rounded bg-inherit my-4" data-testid="card-underline" />
+          )}
           {description && (
-            <>
-              <hr className="border-[1px] rounded bg-inherit" />
-              <p className="flex font-brandLight leading-normal">{description}</p>
-            </>
+            <p className="flex font-brandLight leading-normal" data-testid="card-description">
+              {description}
+            </p>
           )}
         </div>
       </div>
--- src/stories/Card.stories.tsx
+++ src/stories/Card.stories.tsx
@@ -21,10 +21,41 @@ type Story = StoryObj<typeof meta>;
 
 export const _Card: Story = {
   args: {
-    className: 'bg-brand-light-green text-brand-coal',
+    className: 'bg-light-green text-coal',
     icon: <PostImage color="#0f0d0e" />,
     subTitle: '01',
     title,
     description,
+  },
+};
+
+export const NoIcon: Story = {
+  args: {
+    className: 'bg-yellow text-coal',
+    subTitle: '01',
+    title,
+    description,
+  },
+};
+
+export const NoSubTitle: Story = {
+  args: {
+    className: 'bg-pink text-coal',
+    title,
+    description,
+  },
+};
+
+export const NoTitle: Story = {
+  args: {
+    className: 'bg-purple text-white',
+    description,
+  },
+};
+
+export const NoDescription: Story = {
+  args: {
+    className: 'bg-coal text-biege',
+    title,
   },
 };
